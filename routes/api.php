d07df5c8e8da754d9c855807e58d3177f650c12d
tientv <viettien321992@gmail.com>
2020-07-17 17:10:09 +0700

    tientran

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 //});
 Route::get('book/count','BookController@getAllBook');
 /*Route::get('book/add','BookController@create')*/;
-Route::apiResource('book', 'BookController')->middleware('auth.jwt');
+Route::apiResource('book', 'BookController');
 Route::post('login', 'UserController@login');
 
 Route::group(['middleware' => 'auth.jwt'], function () {
